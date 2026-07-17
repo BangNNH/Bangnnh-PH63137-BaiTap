21aaef94536ba9a1a792106e440ba5fd2d422d3b
BangNNH <nnhb15012000@gmail.com>
2026-07-17 19:26:41 +0700

    Lab 5

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,16 +1,62 @@
-import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+}
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule],
+  imports: [RouterLink],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
 export class Products {
-  products = [
-    'Laptop',
-    'Phone',
-    'Tablet'
-  ];
+  products: Product[] = [];
+  isLoading: boolean = true;
+  errorMessage: string | null = null;
+  deletingId: number | null = null;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.isLoading = true;
+    this.errorMessage = null;
+
+    this.http.get<Product[]>('http://localhost:3000/products').subscribe({
+      next: (data) => {
+        this.products = data;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.isLoading = false;
+        this.errorMessage = 'Không thể kết nối máy chủ!';
+      },
+    });
+  }
+
+  deleteProduct(id: number) {
+    const confirmDelete = confirm('Bạn chắc chắn muốn xóa sản phẩm này?');
+    if (!confirmDelete) return;
+
+    this.http.delete(`http://localhost:3000/products/${id}`).subscribe({
+      next: () => {
+        this.products = this.products.filter(
+          (product) => product.id !== id
+        );
+        alert('Xóa sản phẩm thành công!');
+      },
+      error: () => {
+        alert('Xóa sản phẩm thất bại!');
+      },
+    });
+  }
 }
--- src/app/pages/stories/stories.ts
+++ src/app/pages/stories/stories.ts
@@ -1,60 +1,59 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+interface Story {
+  id: number;
+  title: string;
+  author: string;
+  views: number;
+  releaseYear: number;
+  imageUrl: string;
+  genres: string[];
+}
 
 @Component({
   selector: 'app-stories',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './stories.html',
   styleUrl: './stories.css',
 })
 export class Stories {
-  stories = [
-    {
-      title: "One Piece",
-      author: "Oda",
-      views: 100000,
-      year: 1997,
-      category: "Phiêu lưu",
-      image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
-    },
-    {
-      title: "Naruto",
-      author: "Kishimoto",
-      views: 90000,
-      year: 1999,
-      category: "Hành động",
-      image: "https://cdn.myanimelist.net/images/manga/3/117681.jpg",
-    },
-    {
-      title: "Doraemon",
-      author: "Fujiko F. Fujio",
-      views: 70000,
-      year: 1969,
-      category: "Thiếu nhi",
-      image: "https://cdn.myanimelist.net/images/manga/1/181546.jpg",
-    },
-    {
-      title: "Dragon Ball",
-      author: "Akira Toriyama",
-      views: 95000,
-      year: 1984,
-      category: "Hành động",
-      image: "https://cdn.myanimelist.net/images/manga/1/113747.jpg",
-    },
-    {
-      title: "Attack On Titan",
-      author: "Hajime Isayama",
-      views: 85000,
-      year: 2009,
-      category: "Dark Fantasy",
-      image: "https://cdn.myanimelist.net/images/manga/2/37846.jpg",
-    },
-    {
-      title: "Bleach",
-      author: "Tite Kubo",
-      views: 70000,
-      year: 2001,
-      category: "Shounen",
-      image: "https://cdn.myanimelist.net/images/manga/3/179882.jpg",
-    },
-  ];
+  stories: Story[] = [];
+  isLoading: boolean = true;
+  errorMessage: string | null = null;
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    this.getStories();
+  }
+
+  getStories() {
+    this.isLoading = true;
+    this.errorMessage = null;
+    this.http.get<Story[]>(`http://localhost:3000/stories`).subscribe({
+      next: (data) => {
+        this.stories = data;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.isLoading = false;
+        this.errorMessage = 'Không thể kết nối máy chủ!!!! LỖI';
+      },
+    });
+  }
+
+  deleteStory(id: number) {
+    const confirmDelete = confirm('Bạn chắc chắn muốn xóa sản phầm này????');
+    if (!confirmDelete) return;
+
+    this.http.delete(`http://localhost:3000/stories/${id}`).subscribe({
+      next: () => {
+        this.stories = this.stories.filter((story) => story.id !== id);
+        alert('Xóa sản phẩm thành công');
+      },
+      error: () => {
+        alert('Xóa sản phầm thất bại!!!! LỖI');
+      },
+    });
+  }
 }
